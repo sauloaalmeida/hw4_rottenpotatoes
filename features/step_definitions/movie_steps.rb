9fef54b33f71695683e7567ddf1cc2bab54ead55
sauloaalmeida <sauloandrade@gmail.com>
2012-08-14 19:11:51 -0700

    resolvendo exercicios hw4, ainda nao finalizado

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,7 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+  end
+end
